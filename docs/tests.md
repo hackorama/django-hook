d59163d35809406fc865e8f3fbf67e3432d434b0
Kishan Thomas <kishan.thomas@gmail.com>
2020-04-10 17:30:03 -0700

    Adding web app tests

--- docs/tests.md
+++ docs/tests.md
@@ -12,7 +12,7 @@ $ python manage.py test --with-coverage
 nosetests --cover-package=hooks --cover-html --with-coverage --verbosity=1
 Using selector: KqueueSelector
 Creating test database for alias 'default'...
-..
+....
 Name                               Stmts   Miss  Cover
 ------------------------------------------------------
 hooks/__init__.py                      0      0   100%
@@ -22,18 +22,17 @@ hooks/apps.py                          3      3     0%
 hooks/forms.py                         6      0   100%
 hooks/migrations/0001_initial.py       5      0   100%
 hooks/migrations/__init__.py           0      0   100%
-hooks/models.py                       11     11     0%
+hooks/models.py                       11      9    18%
 hooks/serializers.py                  11      0   100%
 hooks/urls.py                          8      0   100%
-hooks/views.py                        72     51    29%
+hooks/views.py                        72     44    39%
 ------------------------------------------------------
-TOTAL                                130     70    46%
+TOTAL                                130     61    53%
 ----------------------------------------------------------------------
-Ran 2 tests in 0.125s
+Ran 4 tests in 0.255s
 
 OK
 Destroying test database for alias 'default'...
-$
 ```
 
 Open interactive HTML reports at `cover/index.html`
--- server/hooks/tests.py
+++ server/hooks/tests.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.test import TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
@@ -9,7 +10,7 @@ from .models import Event, Webhook
 logger = logging.getLogger(__name__)
 
 
-class EventTests(APITestCase):
+class EventAPITests(APITestCase):
 
     def test_create_event(self):
         """
@@ -23,7 +24,7 @@ class EventTests(APITestCase):
         self.assertEqual(Event.objects.get().name, "test-one")
 
 
-class WebhookTests(APITestCase):
+class WebhookAPITests(APITestCase):
 
     def test_create_webhook(self):
         """
@@ -35,3 +36,26 @@ class WebhookTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Webhook.objects.count(), 1)
         self.assertEqual(Webhook.objects.get().name, "test-hook")
+
+
+class WebAppTests(TestCase):
+
+    def test_index_webhook(self):
+        """
+        Test the webhook listing on index page
+        """
+        webhook = Webhook.objects.create(name="test", url="http://test/test")
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.context['webhooks'].count(), 1)
+        self.assertEqual(response.context['webhooks'].first().name, "test")
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_event(self):
+        """
+        Test the event listing on index page
+        """
+        event = Event.objects.create(name="test")
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.context['events'].count(), 1)
+        self.assertEqual(response.context['events'].first().name, "test")
+        self.assertEqual(response.status_code, 200)
